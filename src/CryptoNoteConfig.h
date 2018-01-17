cebf847dad91b9d73abf857f962795eac47380ff
ratana-project <ratanaxyz@gmail.com>
2018-01-17 12:31:49 -0500

    Added Genesis Coinbase TX Hex
    Added Genesis Coinbase TX Hex to src\CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Ratana";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "01d80401ff000104029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101487321b6b45dd7a74b72bc5aa65d5c2fe47bff93957b482f5d70fbe1a92334a1";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
